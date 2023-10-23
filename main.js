0030fa3ab1c7554cbb3f7c21305cc02665dd5c89
PROF ROMULO <rjlpena@gmail.com>
2023-10-23 18:35:41 +0000

    testes

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-alert("Bem vindo a esta página, eu sou um alert, e você pode me excluir se desejar!")
+//const aplausos = document.querySelectorAll(".tecla");
+
+//aplausos[0].onclick = tocaSomAplausos;
+
+//const aplausos = document.querySelector("#som_tecla_aplausos");
+
+
+//function tocaSom(som){
+   // alert(som);
+//}
+//tocaSom("clap clap clap");
+
+
+//let controle = "tocar";
+//if(controle == "tocar"){
+  //  alert("áudio pronto para tocar");
+//}else{
+  //  alert("o áudio não pode ser tocado");
+//}
+
+const audios = document.querySelectorAll("audio");
+const buttons = document.querySelectorAll("button");
+let contador = 0;
+while(contador < buttons.length){
+    buttons[contador]textContext = contador; 
+    contador++;
+}
